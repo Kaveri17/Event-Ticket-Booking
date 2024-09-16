847705b1e15c57ccb539804de34caedb5bf2a812
ealinaa <alinaadhikari108@gmail.com>
2024-09-16 10:39:26 +0545

    frontend setup Alina

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline bg-slate-600">
-      Hello world!
+      Hello world from AKA EVENT TICKET BOOKING SYSTEM
     </h1>
     </>
   )
