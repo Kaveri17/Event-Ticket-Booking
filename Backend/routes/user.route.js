5d5ae713c4e43979df19238d8a51d007d1ab0c6e
kaveripunu <kaveripunu@gmail.com>
2024-10-02 01:00:30 +0545

    Integration Completed, and Email Verification page made

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -9,7 +9,6 @@ router.get("/check-auth", verifyToken,checkAuth)
 router.post("/signup",signup)
 router.post("/login",login)
 router.post("/logout",logout)
-
 router.post("/verify-email",verifyEmail)
 router.post("/forgot-password",forgotPassword)
 router.post("/reset-password/:token",resetPassword)
